f33479221a745c82c5bf393f7886187b0e1dda81
monicahdias <msumiedias@gmail.com>
2022-02-05 16:14:49 -0300

    zerando o placar e ajuste

--- projeto2.js
+++ projeto2.js
@@ -1,15 +1,17 @@
 const prompt = require("prompt-sync")();
 
-const elementos = ["PEDRA", "PAPEL", "TESOURA"];
-let vitorias = 0;
-let derrotas = 0;
-let empates = 0;
-let rodadas = 0;
-let escolhaJogador = "";
+while (true) {
+  const elementos = ["PEDRA", "PAPEL", "TESOURA"];
+  let vitorias = 0;
+  let derrotas = 0;
+  let empates = 0;
+  let rodadas = 0;
+  let escolhaJogador = "";
 
-let jogador = prompt("Insira o nome do jogador: ");
-console.log();
-console.log(`Olá, ${jogador}!
+  console.log();
+  let jogador = prompt("Insira o nome do jogador: ");
+  console.log();
+  console.log(`Olá, ${jogador}!
 
 Vamos jogar Jokenpô?
 
@@ -20,9 +22,7 @@ Papel ganha da pedra!
 
 Será que você será capaz de me vencer?`);
 
-console.log();
-
-while (true) {
+  console.log();
   while (true) {
     rodadas = +prompt(
       `${jogador}, quantas rodadas você gostaria de jogar? `
@@ -52,35 +52,46 @@ while (true) {
         break;
       }
     }
+    console.log();
     let escolhaPc = elementos[Math.floor(Math.random() * elementos.length)];
     console.log(`A escolha do PC foi: ${escolhaPc}
   A sua escolha foi: ${escolhaJogador}`);
+    console.log();
     if (escolhaPc == elementos[0] && escolhaJogador == elementos[1]) {
       console.log("Você ganhou!");
       vitorias++;
+      console.log();
     } else if (escolhaPc == elementos[1] && escolhaJogador == elementos[0]) {
       console.log("O PC ganhou!");
       derrotas++;
+      console.log();
     } else if (escolhaPc == elementos[1] && escolhaJogador == elementos[2]) {
       console.log("Você ganhou!");
       vitorias++;
+      console.log();
     } else if (escolhaPc == elementos[2] && escolhaJogador == elementos[1]) {
       console.log("O PC ganhou!");
       derrotas++;
+      console.log();
     } else if (escolhaPc == elementos[2] && escolhaJogador == elementos[0]) {
       console.log("Você ganhou!");
       vitorias++;
+      console.log();
     } else if (escolhaPc == elementos[0] && escolhaJogador == elementos[2]) {
       console.log("O PC ganhou!");
       derrotas++;
+      console.log();
     } else if (escolhaPc == escolhaJogador) {
       console.log("Empate!");
       empates++;
+      console.log();
     }
   }
+  console.log();
   console.log(
     `${jogador}, você ganhou ${vitorias} vezes, eu ganhei ${derrotas} vezes e empatamos ${empates} vezes!`
   );
+  console.log();
   if (vitorias > derrotas) {
     console.log(`Parabéns ${jogador}! Você venceu com ${vitorias} vitória(s)!`);
   } else if (vitorias < derrotas) {
@@ -92,6 +103,7 @@ while (true) {
       `Parece que empatamos, ${jogador}! Ficamos com ${empates} empate(s)!`
     );
   }
+  console.log();
   console.log(`${jogador}, você gostaria de jogar novamente?`);
   let jogarNovamente = prompt(`Responda [SIM] ou [NAO]: `)
     .trim()
@@ -100,4 +112,4 @@ while (true) {
     break;
   }
 }
-console.clear();
+console.log();
